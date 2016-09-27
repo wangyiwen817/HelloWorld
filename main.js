172d79a49e4b2bdd82ed3a8d594d399b16889de2
unknown <>
2016-09-27 23:50:46 +0800

    Homework-2 更正

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-function DrawPyramid(totalrows) {
-    var currentRow, spaceCount, starCount;
-    var spacestr, starstr, finalstr;
-    for (currentRow = 0; currentRow < totalrows; currentRow++) {
-        spacestr = "";
-        starstr = "";
-        finalstr = "";
-        for (spaceCount = 0; spaceCount < (totalrows - currentRow - 1); spaceCount++) {
-            spacestr = spacestr + " "; //空格
+function Draw(n) {
+    for (var i = 0; i < n; i++) {
+        var str = "";
+        for (var k = 0; k < i; k++) {
+            str += "**";
         }
-        for (starCount = 0; starCount < (2 * currentRow + 1); starCount++) {
-            starstr = starstr + "*"; //*号
-        }
-        finalstr = spacestr + starstr;
-        console.log(finalstr); //两串组合输出
+        str = str.substring(0, str.length - 1);
+        console.log(str);
     }
 }
-var rows = process.argv[2];
-DrawPyramid(parseInt(rows));
+Draw(5);
